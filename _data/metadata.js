a4c80810b91ba68138672c887558bc4dca984743
Kamie <kamfurdev@tuta.io>
2025-03-30 10:54:23 -0700

    git outta here

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Kamie's Blog",
+	url: "https://kamies-blog.netlify.app/",
+	language: "en",
+	description: "I post about my life, projects, and more!",
+	author: {
+		name: "Kamie",
+		email: "kamboydev@tuta.io",
+		url: "https://kamies-blog.netlify.app/about"
+	}
+}
